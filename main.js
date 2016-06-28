84e9cbbe6c172b2fea06552f0d85f9b6e2004817
AndyZhaofeng <zf526637166@gmail.com>
2016-06-28 15:22:45 +0800

    nodeJs基础

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhaofeng on 16/6/28.
+ */
+
+var counter1=require('./utils/counter');
+var counter2=require('./utils/counter');
+
+console.log(counter1.count());
+console.log(counter1.count());
+console.log(counter2.count());
---
+++ utils/counter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhaofeng on 16/6/28.
+ */
+var i=0;
+function count(){
+    return ++i;
+}
+
+exports.count=count;
